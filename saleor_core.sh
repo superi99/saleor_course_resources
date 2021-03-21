f417ffa1b0f8b0874c45e874a9c1406b32e4d01f
superi99 <51145983+superi99@users.noreply.github.com>
2021-03-21 23:24:08 +0700

    Update saleor_core.sh

--- saleor_core.sh
+++ saleor_core.sh
@@ -1,11 +1,11 @@
 cd /home/ubuntu/codes/backend/saleor
 source /home/ubuntu/codes/backend/venv/bin/activate
-export ALLOWED_HOSTS=3.6.136.178
-export ALLOWED_CLIENT_HOSTS=3.6.136.178
+export ALLOWED_HOSTS=203.210.134.169,https://perfume-dashboard.herokuapp.com
+export ALLOWED_CLIENT_HOSTS=203.210.134.169,https://perfume-dashboard.herokuapp.com
 export DEBUG=True
 export SECRET_KEY=123456
-export INTERNAL_IPS=127.0.0.1,3.6.136.178,
-export API_URI=http://3.6.136.178:8000/graphql/
+export INTERNAL_IPS=127.0.0.1,localhost,
+export API_URI=http://203.210.134.169:8000/graphql/,https://perfume-dashboard.herokuapp.com/graphql/
 export APP_MOUNT_URI=/dashboard/
 export DEFAULT_COUNTRY=IN
 export DEFAULT_CURRENCY=INR
